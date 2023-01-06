d3ffa30b6fc8676015869e04fe5c12dd2c90e6f3
jerryxfl <1072059168@qq.com>
2023-01-06 20:10:30 +0800

    fix after stop can't relunch

---
+++ src/main/kotlin/com/jerry/rt/core/http/pojo/Request.kt
@@ -0,0 +1,9 @@
+package com.jerry.rt.core.http.pojo
+
+/**
+* @className: Request
+* @description: TODO 类描述
+* @author: Jerry
+* @date: 2023/1/6:19:47
+**/class Request {
+}
---
+++ src/main/kotlin/com/jerry/rt/core/http/pojo/Response.kt
@@ -0,0 +1,9 @@
+package com.jerry.rt.core.http.pojo
+
+/**
+* @className: Response
+* @description: TODO 类描述
+* @author: Jerry
+* @date: 2023/1/6:19:47
+**/class Response {
+}
