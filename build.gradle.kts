b2a802ab5c2de23389096ba55cd40ebdd455c6b1
jerryxfl <1072059168@qq.com>
2023-02-12 14:41:36 +0800

    support ssl config,and repaire request read

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
 
     //协程
     compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")//todo remove
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")//todo remove
 }
 
 tasks.test {
