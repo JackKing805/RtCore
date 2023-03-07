f7c667b6279d6b760e503a1e357f8abe7c714f33
jerryxfl <1072059168@qq.com>
2023-03-07 22:17:47 +0800

    fix bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     `maven-publish`
 }
 
-val vv = "0.5.4"
+val vv = "0.5.5"
 
 group = "com.jerry"
 version = vv
@@ -20,7 +20,7 @@ dependencies {
 
     //协程
     compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")//todo remove
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")//todo remove
 }
 
 tasks.test {
